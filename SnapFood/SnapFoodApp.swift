55812c8f10fb6bb5f8b9a2a5c02391615a64c1ca
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-06-01 21:51:58 +0800

    Initial Commit

---
+++ SnapFood/SnapFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapFoodApp.swift
+//  SnapFood
+//
+//  Created by Claudio Tendean on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SnapFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
